a5772e1c9d19ab720ffbb835fd5e24659bc90617
1lyasm <il.mystafa@gmail.com>
2023-03-08 02:15:54 +0400

    Update MainActivity.java

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.todo;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteCursorDriver;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteQuery;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -19,13 +25,23 @@ import androidx.annotation.Nullable;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.Key;
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 
 public class MainActivity extends Activity {
     private EditText AddTaskEditText;
     private LinearLayout CheckBoxLinearLayout;
     private TaskData MainTaskData = new TaskData();
+    private String MainAppDataFileName = "ToDoAppData.txt";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +63,18 @@ public class MainActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadPreviousData();
+    }
+
+    @Override
+    protected void onPause() {
+        storeCurrentData();
+        super.onPause();
+    }
+
     public void removeFocusFromAddTaskEditText(View view) {
         AddTaskEditText.clearFocus();
         InputMethodManager HideKeyboardManager =
@@ -63,11 +91,55 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+    public void loadPreviousData() {
+        MainTaskData.clear();
+        SharedPreferences AppDataSharedPreferences =
+                getSharedPreferences(MainAppDataFileName, Context.MODE_PRIVATE);
+        Collection<?> AppDataCollection = 
+                AppDataSharedPreferences.getAll().values();
+        for (Object TaskNameState: AppDataCollection) {
+            String TaskNameStateString = (String) TaskNameState;
+            MainTaskData.addTask(new Task(TaskNameStateString.substring(0,
+                    TaskNameStateString.length() - 1),
+                    Character.digit(
+                            TaskNameStateString.charAt(
+                                    TaskNameStateString.length() - 1), 10)));
+        }
+        CheckBoxLinearLayout.removeAllViews();
+        for (Task T: MainTaskData.getToDoList()) {
+            CheckBox NewCheckBox = new CheckBox(CheckBoxLinearLayout.getContext());
+            NewCheckBox.setText(T.getTaskName());
+            NewCheckBox.setChecked(T.CompletionState == 1);
+            CheckBoxLinearLayout.addView(NewCheckBox);
+        }
+    }
+
+    public void storeCurrentData() {
+        SharedPreferences AppDataSharedPreferences =
+                getSharedPreferences(MainAppDataFileName, Context.MODE_PRIVATE);
+        SharedPreferences.Editor AppDataEditor = AppDataSharedPreferences.edit();
+        int KeyCount = 0;
+        for (Task T: MainTaskData.getToDoList()) {
+            AppDataEditor.putString(MessageFormat.format("{0}", KeyCount),
+                    MessageFormat.format("{0}{1}", T.getTaskName(),
+                            CheckBoxLinearLayout.
+                                    getChildAt(KeyCount).
+                                    createAccessibilityNodeInfo().isChecked() ? 1 : 0d));
+            ++KeyCount;
+        }
+        AppDataEditor.commit();
+    }
 }
 class Task {
     private String TaskName;
+    public int CompletionState = 0;
     public Task(String TaskName) {
         this.TaskName = TaskName;
+    }
+    public Task(String TaskName, int State) {
+        this.TaskName = TaskName;
+        CompletionState = State;
     }
     public String getTaskName() {
         return TaskName;
@@ -87,6 +159,9 @@ class TaskData {
 
     public void addTask(String TaskName) {
         TaskList.add(new Task(TaskName));
+    }
+    public void addTask(Task TaskObject) {
+        TaskList.add(TaskObject);
     }
 
     public boolean removeTaskByValue(String TaskName) {
@@ -111,5 +186,8 @@ class TaskData {
 
     public ArrayList<Task> getToDoList() {
         return TaskList;
+    }
+    public void clear() {
+        TaskList.clear();
     }
 }
